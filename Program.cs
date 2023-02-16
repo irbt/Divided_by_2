86a1ebf13cc5cade4ef5183235228aa7f4867964
Emmanuel Marchal <81331382+irbt@users.noreply.github.com>
2023-02-16 19:45:14 +0100

    exercice

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿//prints out how many times the number can be divided by 2 evenly
+using System;
+
+
+
+namespace Program;
+
+class RecursionTask{
+public static int DividedBy2(double num){
+
+int count = 0;
+
+if (num >0 && num%2 == 0){
+    count++;
+    num/=2;
+
+    return count += DividedBy2(num);
+}
+return count;
+}
+
+public static void Main (string[] args){
+
+Console.WriteLine("Entrez un entier");
+double number = Convert.ToDouble (Console.ReadLine());
+
+int result = DividedBy2(number);
+
+Console.WriteLine($"Le nombre {number} peut etre divisé {result} fois par  2  " );
+
+}
+
+}
